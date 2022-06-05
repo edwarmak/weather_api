ad8f6c1a010fbb829b732af2ffa0d38454d617ca
Makayla Edwards <mackedwards@Makaylas-MacBook-Air.local>
2022-06-05 16:38:58 -0600

    connected API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import axios from 'axios'
+
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY
 
 function App() {
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=${API_KEY}`
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
     </div>
   );
 }
