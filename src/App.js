d453b3b5b391a7c260771e7c60739081b6842623
Makayla Edwards <mackedwards@Makaylas-MacBook-Air.local>
2022-06-05 19:54:05 -0600

    data is displaying

--- src/App.js
+++ src/App.js
@@ -1,40 +1,65 @@
 import React, {useState} from 'react'
 import axios from 'axios'
-
+// access API KEY from .env file
 const API_KEY = process.env.REACT_APP_WEATHER_API_KEY
 
 function App() {
-
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=${API_KEY}`
+  const [data,setData] = useState({})
+  const [location, setLocation] = useState('')
+  // below is the API url with API KEY inserted from .env file in order to push to github without publically displaying API key
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`
+  // search function to change location state upon search
+  const searchLocation = (event) => {
+    // wrap api call in IF statement to submit input on 'Enter'
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data);
+      })
+    }
+  }
   
   return (
     <div className="App">
+      <div className="search">
+        <input
+        value={location}
+        onChange={event => setLocation(event.target.value)}
+        onKeyPress={searchLocation}
+        placeholder="Enter Location"
+        type="text" />
+      </div>
       <div className="container">
         <div className="top">
           <div className="location">
-            <p>Salt Lake City</p>
+            <p>{data.name}</p>
           </div>
           <div className="temp">
-            <h1>65°F</h1>
+            {data.main ? <h1>{data.main.temp}°F</h1> : null}
           </div>
           <div className="description">
-            <p>Clouds</p>
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
           </div>
         </div>
+
+      {data.name != undefined &&
         <div className="bottom">
-         <div className="feels">
-            <p className="bold">65°F</p>
-            <p>Feels Like</p>
-          </div>
-          <div className="humidity">
-            <p className="bold">10%</p>
-            <p>Humidity</p>
-          </div>
-          <div className="wind">
-            <p className="bold">5 MPH</p>
-            <p>Wind Speed</p>
-          </div>
-        </div>
+        <div className="feels">
+           {data.main ? <p className="bold">{data.main.feels_like}</p> : null}
+           <p>Feels Like</p>
+         </div>
+         <div className="humidity">
+           {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
+           <p>Humidity</p>
+         </div>
+         <div className="wind">
+           {data.wind ? <p className="bold">{data.wind.speed} MPH</p> : null}
+           <p>Wind Speed</p>
+         </div>
+       </div>
+      }
+
+
           
       </div>
     </div>
